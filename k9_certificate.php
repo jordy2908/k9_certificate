d9c0637ade8423b83d7cc4f7d48789bc98cf2cdf
jordy2908 <bcarlosjordy@gmail.com>
2023-09-22 01:13:10 -0500

    add

--- k9_certificate.php
+++ k9_certificate.php
@@ -1,10 +1,17 @@
 <?php
+
 /**
-* Plugin Name: K9 Certificate
-* Plugin URI: 
-* Description: Gestiona certificados.
-* Version: 1.0.1
-* Author: JEC inc
-* Author URI:
-* License: MIT
-*/
+ * Plugin Name: K9 certificate
+ * Plugin URI: 
+ * Description: Gestiona certificados.
+ * Version: 1.0.0.1
+ * Author: Jordy
+ * Author URI:
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
+
+// Add a change here
+
+// Add another change from the fork
+
+// Fix the bug
